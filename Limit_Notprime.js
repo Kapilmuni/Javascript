3bb1f7af93b6291c2f362f39382590bccc784d72
Kapilmuni <kapillilhare2@.com>
2024-04-07 15:33:50 +0530

    Non-Primes from 2 to a given limit

---
+++ Limit_Notprime.js
@@ -0,0 +1,16 @@
+function com(num){
+    for(i=1;i<=num;i++){
+        var count=0
+        if(i>1){
+            for(j=1;j<=i;j++){
+                if(i%j==0){
+                    count++
+                }
+            }
+            if(count!=2){
+                console.log(i,"is not prime")
+            }
+        }   
+    }
+}
+com(10)
--- test.js
+++ test.js
@@ -1,19 +1,35 @@
-let obj={
-    name:"ram",
-    lastname:"kumar",
-    print:function(){
-        console.log(this.name,this.lastname)
-    }
-}
-obj.print()
+// let obj={
+//     name:"ram",
+//     lastname:"kumar",
+//     print:function(){
+//         console.log(this.name,this.lastname)
+//     }
+// }
+// obj.print()
 
-function gr(num){
-    for(i=1;i<=num;i++){
-        if(num%i==0){
-            console.log(i)
-        }
+// function gr(num){
+//     for(i=1;i<=num;i++){
+//         if(num%i==0){
+//             console.log(i)
+//         }
          
         
+//     }
+// }
+// console.log(gr(9))
+function com(num){
+    for(i=1;i<=num;i++){
+        var count=0
+        if(i>1){
+            for(j=1;j<=i;j++){
+                if(i%j==0){
+                    count++
+                }
+            }
+            if(count!=2){
+                console.log(i,"is not prime")
+            }
+        }   
     }
 }
-console.log(gr(9))
+com(50)
